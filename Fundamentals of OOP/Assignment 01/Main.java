e3beb76f87ba9b9b904463534d3642554d042c7e
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-09-14 15:00:17 +0530

    Assignment 01

---
+++ Fundamentals of OOP/Assignment 01/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Date d1 = new Date();
+        d1.display();
+
+        Date d2 = new Date(29, 10, 1999);
+        d2.display();
+
+        d2.getDate().display();
+
+        d2.displayWithFormat('-');
+        d2.displayWithFormat('/');
+        d2.displayWithFormat('.');
+    }
+}
